e34531aa7cc7ec8c703d0b991930cd6c23484e04
rollicha <54882495+rollicha@users.noreply.github.com>
2020-05-22 13:57:08 +0300

    changed to d3.js v4 and to get instead of then function

--- builds/d3/lineCSV.js
+++ builds/d3/lineCSV.js
@@ -2,40 +2,47 @@ function drawChart(){
 
 
     //d3.v5 versionen
-    d3.csv("lineData.csv").then(function(data){
-            console.log(data);
+    //d3.csv("lineData.csv").then(function(data)
+    d3.csv("lineData.csv").get(function (error,data)    {
+            //console.log(data);
             //document.write (JSON.stringify(data));
 
             //ladda in data
-            var temps = [], months = [];
+            var temps = [], months = [], dataFix = [];
             for(i=0; i<data.length;i++){
                 months.push(data[i].Month);
-                temps.push(data[i].Temp);
+                temps.push(parseFloat (data[i].Temp));
+                dataFix.push({month:months[i], temp:temps[i]})
             }
                 console.log(months);
                 console.log(temps);
+                console.log(dataFix);
 
                       //skapa ritunderlag
 
-        var width = 800, height = 500;
+        var width = 600, height = 300;
         var canvas = d3.select('body').append('svg').attr('width', width).attr('height', height);
 
 
+            var xScale = d3.scaleBand()
+                .domain(months)
+                .range([0,width]);
 
             var yScale = d3.scaleLinear()
                 .domain([d3.min(temps), d3.max(temps)])   //vilka värden konverteras till pixelvärden
-                .range([0,height]);   //pixelvärden ska läggas mellan vilka värden
+                .range([height,0]);   //pixelvärden ska läggas mellan vilka värden
+
 
         //genererar linje för path
         var dString = d3.line()
-            .x(function(data) { return data.Month })
-            .y(function(data) { return yScale(data.Temp) });
-            console.log(dString(data));
+            .x(function(d) { return xScale(d.month) })
+            .y(function(d) { return yScale(d.temp) });
+
 
             canvas.append('path')
-                .attr('fill, none')
-                .attr('stroke','blue')
-                .attr('d', dString(data));
+                .attr('fill', 'none')
+                .attr("stroke","blue")
+                .attr('d', dString(dataFix));
     
                 
     });
